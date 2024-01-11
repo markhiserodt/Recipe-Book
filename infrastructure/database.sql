952e552089c12538ffa062d829a15f1f4a95cbb4
markhiserodt-avanade <mark.hiserodt@avanade.com>
2024-01-11 13:12:09 -0800

    update sql

--- infrastructure/database.sql
+++ infrastructure/database.sql
@@ -1,36 +1,13 @@
-CREATE TABLE Food (
-	id			INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	name		VARCHAR(255)	NOT NULL,
-);
-GO
-
-INSERT INTO Food (name)
-VALUES			 ('Bread');
-GO
-
-INSERT INTO Food (name)
-VALUES			 ('Eggs'),
-				 ('Chicken'),
-				 ('Potatoes'),
-				 ('Milk'),
-				 ('Spinach'),
-				 ('Onions'),
-				 ('Bananas'),
-				 ('Cereal');
-GO
-
 CREATE TABLE Food_Group (
 	id			INT				NOT NULL	IDENTITY	PRIMARY KEY,
 	name		VARCHAR(255)	NOT NULL,
 );
 GO
 
-INSERT INTO Food_Group (name)
-VALUES			 ('Grains'),
-				 ('Dairy'),
-				 ('Poultry'),
-				 ('Vegetables'),
-				 ('Fruits');
+CREATE TABLE Food (
+	id			INT				NOT NULL	IDENTITY	PRIMARY KEY,
+	name		VARCHAR(255)	NOT NULL,
+);
 GO
 
 ALTER TABLE Food ADD
@@ -40,28 +17,6 @@ ALTER TABLE Food ADD
 	REFERENCES Food_Group (id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE;
-GO
-
-UPDATE Food
-SET food_group_id = (SELECT id FROM Food_Group WHERE name = 'Grains')
-WHERE (name = 'Potatoes' OR name = 'Cereal');
-
-UPDATE Food
-SET food_group_id = (SELECT id FROM Food_Group WHERE name = 'Dairy')
-WHERE (name = 'Eggs' OR name = 'Milk');
-
-UPDATE Food
-SET food_group_id = (SELECT id FROM Food_Group WHERE name = 'Poultry')
-WHERE (name = 'Chicken');
-
-UPDATE Food
-SET food_group_id = (SELECT id FROM Food_Group WHERE name = 'Vegetables')
-WHERE (name = 'Spinach' OR name = 'Onions');
-GO
-
-UPDATE Food
-SET food_group_id = (SELECT id FROM Food_Group WHERE name = 'Fruits')
-WHERE (name = 'Bananas');
 GO
 
 CREATE TABLE Recipe (
@@ -84,16 +39,17 @@ ALTER TABLE Recipe_Food ADD
 	CONSTRAINT PK_RF PRIMARY KEY (recipe_id, food_id);
 GO
 
-INSERT INTO Recipe (name)
-VALUES ('Chicken Salad'),
-	   ('Breakfase Burrito');
+CREATE TABLE Recipe_Comment (
+	id			INT				NOT NULL	IDENTITY	PRIMARY KEY,
+	comment		VARCHAR(255)	NOT NULL,
+);
 GO
 
-INSERT INTO Recipe_Food (recipe_id, food_id)
-VALUES  (1, 3),
-		(1, 6),
-		(1, 7),
-		(2, 2),
-		(2, 4),
-		(2, 1);
+ALTER TABLE Recipe_Comment ADD
+	recipe_id INT NOT NULL,
+	CONSTRAINT FK_Recipe
+	FOREIGN KEY (recipe_id)
+	REFERENCES [dbo].[Recipe] (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE;
 GO
